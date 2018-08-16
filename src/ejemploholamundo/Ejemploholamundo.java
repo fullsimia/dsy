8e458695efeeac4ed63e83d63d060f6bda369719
LC1301312 <LC1301312>
2018-08-16 10:04:48 -0400

    lo subi

---
+++ src/ejemploholamundo/Ejemploholamundo.java
@@ -0,0 +1,18 @@
+
+package ejemploholamundo;
+
+/**
+ *
+ * @author Duoc
+ */
+public class Ejemploholamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Ejemplo para GitHub / GitLab");
+    }
+    
+}
